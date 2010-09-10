eeb9f1f74423eea542dda652daffbe44bbe0737b
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-09-10 11:35:32 +0200

    cleaner quantiles storing-retrieving, better output dir default

--- apt-R-preprocess.R
+++ apt-R-preprocess.R
@@ -6,13 +6,16 @@ if(!suppressPackageStartupMessages(require("optparse", quietly=TRUE))) {
 option_list <-
   list(
        make_option(c("-d", "--cdf"), help="CDF file name"),
-       make_option(c("-q", "--load-quantiles-from"), help="load quantiles from specified file"),
+       make_option(c("-l", "--load-quantiles"), action="store_true", default=FALSE,
+                   help="load previously computed quantiles, don't recompute them [default: FALSE]"),
+       make_option(c("-q", "--quantiles-file"), default="quantiles.txt",
+                   help="quantiles file name [default: quantiles.txt]"),
        make_option("--no-rma", action="store_false", dest="rma",
                    help="skip RMA preprocessing (not yet implemented)"),
        make_option(c("-o", "--output-file"), default="X.RData",
                    help="output RData file name [default: X.RData]"),
-       make_option("--output-folder", default=".",
-                   help="output folder where to store logs and extra analysis results [default: current directory]")
+       make_option("--output-folder", default="",
+                   help="output folder where to store logs and extra analysis results [default: trash everything]")
        )
 
 parser <- OptionParser(usage="%prog [options] cel-files", option_list=option_list)
@@ -30,8 +33,11 @@ if(is.null(opt$cdf)) {
 }
 
 outputDir <- opt$`output-folder`
+if(outputDir=="") {
+  outputDir <- tempdir()
+}
 cmd <- paste("apt-probeset-summarize -a rma-sketch -o", outputDir,
-             if(!is.null(opt$`load-quantiles-from`)) paste("--target-sketch", opt$`load-quantiles-from`) else "--write-sketch",
+             if(opt$`load-quantiles`) paste("--target-sketch", opt$`quantiles-file`) else "--write-sketch",
              "-d", opt$cdf,
              paste(celFiles, collapse=" "))
 aptVersion <- gsub("^version: apt-(.*?) .*", "\\1",
@@ -40,6 +46,7 @@ message("using Affymetrix Power Tools, version ", aptVersion)
 message("console command:")
 message(sQuote(cmd))
 
+quantilesOriginalFileName <- file.path(outputDir, "rma-bg.quant-norm.normalization-target.txt")
 txtOut <- file.path(outputDir, "rma-sketch.summary.txt")
 system(cmd)
 txtTemp <- tempfile()
@@ -47,3 +54,9 @@ system(paste("grep --invert-match ^#%.*", txtOut, ">", txtTemp))
 X <- read.delim(txtTemp, sep="\t", comment.char="", as.is=TRUE)
 X <- structure(t(as.matrix(X[,-1])), dimnames=list(colnames(X)[-1], X[,1]))
 save(X, file="X.RData")
+
+if(!opt$`load-quantiles`) {
+  if(!file.copy(quantilesOriginalFileName, opt$`quantiles-file`, overwrite=TRUE)) {
+    stop("can't store the quantiles file")
+  }
+}
